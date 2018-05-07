2ec22f6aa05900243c447500d08a4c24114588fa
Randolph <randolphl@zylun.com>
2018-05-07 06:02:21 +0000

    Example for non-blocking route handler

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,6 +11,26 @@ public func routes(_ router: Router) throws {
         return "Hello, world!"
     }
 
+    //Non-blocking "Hello, world!" example
+    router.get("helloworld") { req -> Future<String> in
+        /// Create a new void promise
+        let promise = req.eventLoop.newPromise(String.self)
+
+        /// Dispatch some work to happen on a background thread
+        DispatchQueue.global(qos: .background).async(execute: {
+            /// Puts the background thread to sleep
+            /// This will not affect any of the event loops
+            sleep(5)
+
+            /// When the "blocking work" has completed,
+            /// complete the promise and its associated future.
+            promise.succeed(result: "Hello, world!")
+        })
+
+        /// Wait for the future to be completed,
+        return promise.futureResult
+    }
+
     // Example of creating a Service and using it.
     router.get("hash", String.parameter) { req -> String in
         let string = try req.parameters.next(String.self)
