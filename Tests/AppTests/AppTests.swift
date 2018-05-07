ebeacae5ec4a8ea3faae7b411262edd362000290
Randolph <randolphl@zylun.com>
2018-05-07 00:48:05 +0000

    Add Unit Test and Circle CI integration

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -1,13 +1,16 @@
 import App
 import Dispatch
 import XCTest
+import Crypto
 
 final class AppTests: XCTestCase {
-    func testNothing() throws {
-        XCTAssert(true)
+    func testBCrypt() throws {
+        let hash = try BCrypt.hash("randolph", cost: 6)
+        let verify = try BCrypt.verify("randolph", created: hash)
+        XCTAssert(verify)
     }
 
     static let allTests = [
-        ("testNothing", testNothing)
+        ("testBCrypt", testBCrypt)
     ]
 }
