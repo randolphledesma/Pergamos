4270412c683227453d7480b2677343b89b867627
Randolph <randolphl@zylun.com>
2018-05-07 01:56:15 +0000

    update README

--- Package.swift
+++ Package.swift
@@ -1,16 +1,1 @@
-// swift-tools-version:4.1
-import PackageDescription
-
-let package = Package(
-    name: "pergamos",
-    dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.1")
-    ],
-    targets: [
-        .target(name: "App", dependencies: ["Vapor"]),
-        .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App"])
-    ]
-)
-
+/projects/pergamos/Package.swift
